25e01ab31844d10f53ccb653b3c20847e157d0d0
ziwoo <133856471+ziw-oo@users.noreply.github.com>
2023-10-25 12:43:34 +0900

    Update 1025.js

--- 1025.js
+++ 1025.js
@@ -88,3 +88,65 @@ fruits.push('🍎');
 console.log(fruits);
 console.log(fruits.indexOf('🍎'));
 console.log(fruits.lastIndexOf('🍎'));
+
+
+
+// quiz
+// 1. make a string out of an array
+{
+    const fruits = ['apple', 'banana', 'orange'];
+    const result = fruits.join(',');
+    console.log(result);
+}
+
+// 2. make an array out of a string
+{
+    const fruits = '🍎, 🥝, 🍌, 🍒';
+    const result = fruits.split();
+    console.log(result);
+}
+
+// 3. make this array look like this: [5, 4, 3, 2, 1]
+{
+    const array = [1, 2, 3, 4, 5];
+    const result = array.reverse();
+    console.log(result);
+    console.log(array);
+}
+
+// 4. make new array without the first two elements
+{
+    const array = [1, 2, 3, 4, 5];
+    const result = array.slice(2, 5);
+    console.log(result);
+    console.log(array);
+}
+
+
+
+//
+class Student {
+    constructor(name, age, enrolled, score) {
+        this.name = name;
+        this.age = age;
+        this.enrolled = enrolled;
+        this.score = score;
+    }
+}
+const students = [
+    new Student('A', 29, true, 45),
+    new Student('B', 28, false, 80),
+    new Student('C', 30, true, 90),
+    new Student('D', 40, false, 60),
+    new Student('E', 18, true, 88),
+];
+
+// 5. find a stuent with the score 90
+{
+    const result = students.find(function (student, index) {
+        console.log(student, index);
+    });
+// or 
+//    const result = students.find((student) => student.score === 90);
+//    console.log(result);
+}
